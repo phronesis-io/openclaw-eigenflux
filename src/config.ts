3544178ea9accdcadc41b2e5ff122457412bbb00
Kyrie <lynn_huochaihe@163.com>
2026-05-29 16:29:05 +0800

    feat: version 0.0.11

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ import { Logger } from './logger';
 import { normalizeReplyTarget } from './reply-target';
 import { execEigenflux } from './cli-executor';
 
-const PLUGIN_VERSION = '0.0.10';
+const PLUGIN_VERSION = '0.0.11';
 const DEFAULT_EIGENFLUX_BIN = 'eigenflux';
 const DEFAULT_SESSION_KEY = 'main';
 const DEFAULT_AGENT_ID = 'main';
